1819d5e8f1ed6fd4b62e528ebadc566e020c86cb
Tyler Newman <100226151+tnichole15@users.noreply.github.com>
2022-05-09 19:25:30 -0400

    Delete Phase02 directory

--- Phase02/newman-sas/public/salamanders/create.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require_once('../../private/initialize.php');
-
-if (is_post_request()) {
-  $salamanderName = $_POST['salamanderName'];
-  echo "Salamander Name: $salamanderName";
-}
--- Phase02/sas/index.php
+++
@@ -1,1 +0,0 @@
-<?php /*header('Location: https://web182.charliewallin.com/sas/public/'); ?>
